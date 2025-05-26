20b6db39a8fcf5f7a97e9d1446682217209f6474
Blurix23 <himansh2096@..12>
2025-05-26 14:53:52 +0530

    zfdf

--- Frontend/src/pages/Landing_page/landing_page.jsx
+++ Frontend/src/pages/Landing_page/landing_page.jsx
@@ -174,6 +174,9 @@ const test = () => {
               <li>
                 <a href="/contact">Contact</a>
               </li>
+               <li>
+                <a href="/about">About</a>
+              </li>
               <li>
                 {/* <a href="/contact">FAQs</a> */}
               </li>
