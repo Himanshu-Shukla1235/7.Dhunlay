f3218c02bdda1b9c62ff589dd52eb93d43500f66
Aman Gubrele <amgubrele01@gmail.com>
2025-07-12 11:06:47 +0530

    message box done

--- Frontend/src/pages/Message/Message.jsx
+++ Frontend/src/pages/Message/Message.jsx
@@ -30,7 +30,11 @@ export default function MessageList() {
           padding: "20px",
         }}
       >
-      <div className="message-main-sec-2"></div>
+      <div className="message-main-sec-2">
+        <div className="div">
+          MESSages
+        </div>
+      </div>
         <div className="message-main-sec-1">
           {" "}
           {messages.map((item) => {
